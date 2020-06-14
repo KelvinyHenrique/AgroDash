bdffd5b213e945a3f962e352002a307b8821504b
Kelviny H <kelvinyhenrique17@gmail.com>
2020-06-14 11:37:22 -0300

    first commit

---
+++ src/screens/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+function Home () {
+    return (
+        <div className="container">
+            <div className="dashboard">
+                <nav className="navbar">
+
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
